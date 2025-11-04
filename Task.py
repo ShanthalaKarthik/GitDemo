1cb3f6e4db2221e7a179dd0a7a32e71cf65005d2
ShanthalaKarthik <shantalakamat@gmail.com>
2025-11-04 23:03:12 +0530

    Add basic arithmetic operations in Task.py

---
+++ Task.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+sum=a+b
+diff=a-b
+mult=a*b
+div=a/b
+print("Addition:",sum)
+print("Subtraction:",diff)
+print("Multiplication:",mult)
+print("Division:",div)
